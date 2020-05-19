118ae696cb7b1fe36a09a6b3132625815b3df6ff
afroprogrammer <forsonfranklin@gmail.com>
2020-05-19 23:11:09 +0000

    Compound Assignment With Augmented Division challenge completed

---
+++ compound-assignment-with-augmented-division.js
@@ -0,0 +1,16 @@
+//Convert the assignments for a, b, and c to use the Augmented Division operator.
+/****HINT****
+a should equal 4.
+b should equal 27.
+c should equal 3.
+You should use the Augmented Division operator for each variable.
+ */
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /=12;
+b /=4;
+c /=11;
