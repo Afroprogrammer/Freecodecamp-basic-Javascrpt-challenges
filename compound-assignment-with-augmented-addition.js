cc3a709252cc8f4c6d84f2605e62303ae3b2c3c9
afroprogrammer <forsonfranklin@gmail.com>
2020-05-19 23:00:20 +0000

    Compound Assignment With Augmented Addition challenge passed

---
+++ compound-assignment-with-augmented-addition.js
@@ -0,0 +1,20 @@
+//Convert the assignments for a, b, and c to use the Augmented Addition operator.
+/***HINT****
+a should equal 15.
+Passed
+b should equal 26.
+Passed
+c should equal 19.
+You should use the Augmented Addition operator for each variable.
+Passed
+You should not modify the code above the specified comment.
+ */
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9 ;
+c += 7;
