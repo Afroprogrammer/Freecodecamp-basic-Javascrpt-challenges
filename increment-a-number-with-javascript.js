5442cbee2f46ed8ea78b017b18185ff1a22f4fff
afroprogrammer <forsonfranklin@gmail.com>
2020-05-19 22:29:47 +0000

    Increment a Number with JavaScript challenge passed

---
+++ increment-a-number-with-javascript.js
@@ -0,0 +1,11 @@
+//Change the code to use the ++ operator on myVar.
+/**HINT*********
+myVar should equal 88.
+You should not use the assignment operator.
+You should use the ++ operator.
+You should not change code above the specified comment.
+ */
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
