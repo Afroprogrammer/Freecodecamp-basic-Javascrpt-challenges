f7d9fde0d0687ef4da491bdb0bd572133f6c3565
afroprogrammer <forsonfranklin@gmail.com>
2020-05-19 23:04:48 +0000

    Compound Assignment With Augmented Subtraction challenge passed

---
+++ compound-assignment-with-augmented-subtraction.js
@@ -0,0 +1,18 @@
+//Convert the assignments for a, b, and c to use the Augmented Subtraction operator.
+/***HINT***
+ a should equal 5.
+b should equal -6.
+c should equal 2.
+You should use the Augmented Subtraction operator for each variable.
+You should not modify the code above the specified comment.
+ */
+
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -=1;
