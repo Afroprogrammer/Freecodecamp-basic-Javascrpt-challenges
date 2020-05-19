4d7a1094da97d10c160e8e5f6c2509531a5a7b9e
afroprogrammer <forsonfranklin@gmail.com>
2020-05-19 23:07:54 +0000

    Compound Assignment With Augmented Multiplication challenge passed

---
+++ compound-assignment-with-augmented-multiplication.js
@@ -0,0 +1,17 @@
+//Convert the assignments for a, b, and c to use the Augmented Multiplication operator.
+/***HINT ****
+ a should equal 25.
+b should equal 36.
+c should equal 46.
+You should use the Augmented Multiplication operator for each variable.
+You should not modify the code above the specified comment.
+ */
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
