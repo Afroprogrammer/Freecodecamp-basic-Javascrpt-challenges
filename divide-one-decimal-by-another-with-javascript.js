e3151d78956795696f2fe34af76fc6822e5c1838
afroprogrammer <forsonfranklin@gmail.com>
2020-05-19 22:50:07 +0000

    Divide One Decimal by Another with JavaScript challenge passed

---
+++ divide-one-decimal-by-another-with-javascript.js
@@ -0,0 +1,10 @@
+//Change the 0.0 so that  will equal to 2.2.
+/***HINT ****
+The variable  should equal 2.2
+You should use the / operator to divide 4.4 by 2
+ */
+
+var quotient = 4.4 / 2.0; // Change this line
+
+
+
