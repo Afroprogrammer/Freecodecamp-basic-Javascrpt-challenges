14d06af8f0a315088c794b82552a446aed536867
afroprogrammer <forsonfranklin@gmail.com>
2020-05-19 01:22:14 +0000

    solved the challenge by creating a multiline and and inline comment

---
+++ Comment Your JavaScript.js
@@ -0,0 +1,10 @@
+// You should create a // style comment that contains at least five letters.
+// You should create a /* */ style comment that contains at least five letters.
+
+// ANSWER 
+
+
+// this is an in-line comment
+
+/* this is a multiline commment
+use it often*/
