0d2a8830f2f32eba629860ad44c72ec0353a6726
afroprogrammer <forsonfranklin@gmail.com>
2020-05-19 22:17:49 +0000

    Subtract One Number from Another with JavaScript Challenge Passed

---
+++ subtract-one-number-from-another-with-javascript.js
@@ -0,0 +1,10 @@
+
+//Change the 0 so the difference is 12.
+
+/***********HINT ***********
+The variable difference should be equal to 12.
+You should only subtract one number from 45.*/
+
+
+
+var difference = 45 - 33;
