edcbb891706b9320449ab4c466b9f4fbbce91ef9
afroprogrammer <forsonfranklin@gmail.com>
2020-05-20 23:53:04 +0000

    Declare String Variables challenge Passed

---
+++ declare-string-variables.js
@@ -0,0 +1,7 @@
+//Create two new string variables: myFirstName and myLastNamee and assign them the values of your first and last name, respectively.
+/****HINT***
+myFirstName should be a string with at least one character in it.
+myLastName should be a string with at least one character in it.
+ */
+var myFirstName = "Franklin";
+var myLastName = "Oduro";
