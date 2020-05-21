563f15c4ce14a733ee3d8aed06983dde9db09df4
afroprogrammer <forsonfranklin@gmail.com>
2020-05-21 23:16:28 +0000

    Manipulate Arrays With push() challenge passed

---
+++ manipulate-arrays-with-push.js
@@ -0,0 +1,12 @@
+//Push ["dog", 3] onto the end of the myArray variable.
+
+/**HINT**
+ myArray should now equal [["John", 23], ["cat", 2], ["dog", 3]].
+ */
+
+
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
+// Only change code below this line
