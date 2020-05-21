99c0f5650217a9f29942ea9bc6755a7e63e41e56
afroprogrammer <forsonfranklin@gmail.com>
2020-05-21 22:17:47 +0000

    Appending Variables to Strings challenge passed

---
+++ appending-variables-to-strings.js
@@ -0,0 +1,12 @@
+//et someAdjective and append it to myStr using the += operator.
+/**HINT**
+someAdjective should be set to a string at least 3 characters long.
+You should append someAdjective to myStr using the += operator.
+ */
+
+
+// Change code below this line
+
+var someAdjective = "Should be very much fun ";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
