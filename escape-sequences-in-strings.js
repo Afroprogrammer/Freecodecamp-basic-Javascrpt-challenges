a2a7842d35f2829f0a941540393f0cd51bb08716
afroprogrammer <forsonfranklin@gmail.com>
2020-05-21 15:06:45 +0000

    Escape Sequences in Strings challenge passed

---
+++ escape-sequences-in-strings.js
@@ -0,0 +1,29 @@
+/*Assign the following three lines of text into the single variable myStr using escape sequences.
+
+FirstLine
+    \SecondLine
+ThirdLine
+You will need to use escape sequences to insert special characters correctly. You will also need to follow the spacing as it looks above, with no spaces between escape sequences or words.
+
+Here is the text with the escape sequences written out.
+
+FirstLinenewlinetabbackslashSecondLinenewlineThirdLine
+
+*/ 
+            /**HINT***
+ myStr should not contain any spaces
+Passed
+myStr should contain the strings FirstLine, SecondLine and ThirdLine (remember case sensitivity)
+Passed
+FirstLine should be followed by the newline character \n
+Passed
+myStr should contain a tab character \t which follows a newline character
+Passed
+SecondLine should be preceded by the backslash character \
+Passed
+There should be a newline character between SecondLine and ThirdLine
+Passed
+myStr should only contain characters shown in the instructions
+*/
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
