640bb31b4a78610ac6aeb6c0c6a48af7b6fdaecf
afroprogrammer <forsonfranklin@gmail.com>
2020-05-21 22:59:13 +0000

    Nest one Array within Another Array challenge passed

---
+++ nest-one-array-within-another-array.js
@@ -0,0 +1,8 @@
+//Create a nested array called myArray.
+
+/**HINT** 
+myArray should have at least one array nested within another array.
+*/
+
+// Only change code below this line
+var myArray = [['Red','Bulls'], ['Chicago', 'Fre']];
