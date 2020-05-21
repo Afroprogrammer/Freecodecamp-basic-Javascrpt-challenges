d16f01c957a5133bc848474eedf94eedf7b56a95
afroprogrammer <forsonfranklin@gmail.com>
2020-05-21 23:26:58 +0000

    Manipulate Arrays With unshift() challenge passed

---
+++ manipulate-arrays-with-unshift.js
@@ -0,0 +1,13 @@
+
+//Add ["Paul",35] to the beginning of the myArray variable using unshift().
+
+
+/**HINT** 
+ myArray should now have [["Paul", 35], ["dog", 3]].
+*/
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul",35]);
