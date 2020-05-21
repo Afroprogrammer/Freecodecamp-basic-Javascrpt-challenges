725bd96ba1b5efd10b0e2bb622f5f3c78365ded4
afroprogrammer <forsonfranklin@gmail.com>
2020-05-21 23:42:00 +0000

    Write Reusable JavaScript with Functions challenge passed

---
+++ write-reusable-javascript-with-functions.js
@@ -0,0 +1,16 @@
+/**
+Create a function called reusableFunction which prints "Hi World" to the dev console.
+Call the function.
+ */
+
+/**HINT** 
+reusableFunction should be a function.
+reusableFunction should output "Hi World" to the dev console.
+You should call reusableFunction after you define it.
+*/
+
+function reusableFunction(){
+  console.log("Hi World");
+}
+
+reusableFunction();
