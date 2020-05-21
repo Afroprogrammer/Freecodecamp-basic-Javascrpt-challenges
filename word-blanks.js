dbef25ca8f6d4ce35005240ec98e273ca009fc4b
afroprogrammer <forsonfranklin@gmail.com>
2020-05-21 22:52:29 +0000

    Word blanks challenge passed

---
+++ word-blanks.js
@@ -0,0 +1,21 @@
+/**
+In this challenge, we provide you with a noun, a verb, an adjective and an adverb. You need to form a complete sentence using words of your choice, along with the words we provide.
+You will need to use the string concatenation operator + to build a new string, using the provided variables: myNoun, myAdjective, myVerb, and myAdverb. You will then assign the formed string to the wordBlanks variable. You should not change the words assigned to the variables.
+You will also need to account for spaces in your string, so that the final sentence has spaces between all the words. The result should be a complete sentence.
+ */
+
+/**HINT** 
+wordBlanks should be a string.
+You should not change the values assigned to myNoun, myVerb, myAdjective or myAdverb.
+You should not directly use the values "dog", "ran", "big", or "quickly" to create wordBlanks.
+wordBlanks should contain all of the words assigned to the variables myNoun, myVerb, myAdjective and myAdverb separated by non-word characters (and any additional words in your madlib).
+*/
+
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "My  " + myNoun + " is a very " + myAdjective + " can " + myVerb + " very " + myAdverb + " enough "; // Change this line
+// Only change code above this line
