a9c738039f5ef1a2d9a35b695bd574236fb95164
afroprogrammer <forsonfranklin@gmail.com>
2020-05-22 12:47:21 +0000

    Understanding Boolean Values challenge passed

---
+++ understanding-boolean-values.js
@@ -0,0 +1,17 @@
+//Modify the welcomeToBooleans function so that it returns true instead of false when the run button is clicked.
+
+/**HINT** 
+The welcomeToBooleans() function should return a boolean (true/false) value.
+welcomeToBooleans() should return true. 
+*/
+
+
+
+function welcomeToBooleans() {
+
+  // Only change code below this line
+
+  return true; // Change this line
+
+  // Only change code above this line
+}
