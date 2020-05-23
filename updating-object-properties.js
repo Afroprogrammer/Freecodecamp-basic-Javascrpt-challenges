137cadb90971343a1ed2ac2d43c571b3f0f221bb
afroprogrammer <forsonfranklin@gmail.com>
2020-05-23 22:40:03 +0000

    Updating Object Properties challenge completed

---
+++ updating-object-properties.js
@@ -0,0 +1,18 @@
+//Update the myDog object's name property. Let's change her name from "Coder" to "Happy Coder". You can use either dot or bracket notation.
+
+/**HINT**
+You should update myDog's "name" property to equal "Happy Coder".
+You should not edit the myDog definition.
+ */
+
+
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog.name = "Happy Coder";
+// Only change code below this line
