60a829e4fd46f5cd7adeefb2649aef6a1a9f0f45
afroprogrammer <forsonfranklin@gmail.com>
2020-05-23 20:23:21 +0000

    Returning Boolean Values from Functions challenge completed

---
+++ returning-boolean-values-from-functions.js
@@ -0,0 +1,17 @@
+//Fix the function isLess to remove the if/else statements.
+
+/**HINT** 
+isLess(10,15) should return true
+Passed
+isLess(15,10) should return false
+Passed
+You should not use any if or else statements
+*/
+
+
+function isLess(a, b) {
+  // Only change code below this line
+  return a < b;
+}
+
+isLess(10, 15);
