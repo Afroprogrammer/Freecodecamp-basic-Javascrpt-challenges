5efb7af1430d81d2beea0c2ec90e8fb238672d61
afroprogrammer <forsonfranklin@gmail.com>
2020-05-24 19:00:53 +0000

    Iterate with JavaScript For Loops challenge completed

---
+++ iterate-with-javascript-for-loops.js
@@ -0,0 +1,17 @@
+//Use a for loop to work to push the values 1 through 5 onto myArray.
+
+/**HINT** 
+You should be using a for loop for this.
+myArray should equal [1,2,3,4,5].
+*/
+
+
+
+
+
+// Setup
+var myArray = [];
+// Only change code below this line
+for (var i = 1; i <= 5; i++){
+  myArray.push(i);
+}
